640a6f8e6b2890c0c524567121aad1152384be85
Lyon <22521025@ms.uit.edu.vn>
2023-11-26 20:55:58 +0700

    #Lyon

---
+++ Program/QLKS/Form1.cs
@@ -0,0 +1,17 @@
+namespace QLKS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LG_Login_BT_Click(object sender, EventArgs e)
+        {
+            /*
+             * 
+             */
+        }
+    }
+}
