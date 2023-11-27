f131bf706f9e73a71e70dd5daee302e662a81a10
dupper15 <nguyenchinghia15@gmail.com>
2023-11-27 13:56:07 +0700

    #fuckface add hotel

---
+++ Hotel/Hotel/All user control/UC_CheckOut.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel.All_user_control
+{
+    public partial class UC_CheckOut : UserControl
+    {
+        function fn = new function();
+        string query;
+        public UC_CheckOut()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UC_CheckOut_Load(object sender, EventArgs e)
+        {
+            query = "select customer.cid, customer.cname, customer.mobile, customer.nationality, customer.gender, customer.dob, customer.idproof, customer.address, customer.address, customer.checkin, rooms.roomNo, rooms.roomType, rooms.bed, rooms.price from customer inner join rooms on customer.roomid = rooms.roomid where checkout ='NO'";
+
+        }
+    }
+}
---
+++ Hotel/Hotel/Dashboard.cs
@@ -0,0 +1,61 @@
+﻿using Hotel.All_user_control;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            uC_Addroom1.Visible = false;
+            uC_CustomerRes1.Visible = false;
+            uC_CustomerRes1.Visible=false;
+            btAddroom.PerformClick();
+        }
+
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            panelMoving.Left = btAddroom.Left + 50;
+            uC_Addroom1.Visible = true;
+            uC_Addroom1.BringToFront();
+        }
+
+        
+        private void uC_Addroom1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCustomerReg_Click(object sender, EventArgs e)
+        {
+            panelMoving.Left = btCustomerReg.Left + 60;
+            uC_CustomerRes1.Visible = true;
+            uC_CustomerRes1.BringToFront();
+        }
+
+        private void btCheckOut_Click(object sender, EventArgs e)
+        {
+            panelMoving.Left = btCustomerReg.Left + 50; 
+            uC_CustomerRes1.Visible = true;
+            uC_CustomerRes1.BringToFront();
+        }
+    }
+}
